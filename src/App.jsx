bbe32fd1df28b786266aae1dcd141b080cf07437
Chase Crowder <gcrowder93@gmail.com>
2022-01-31 14:45:55 -0800

    colors showing on screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,35 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 import styles from './App.css';
 
 function RGB() {
+  const { r, g, b } = useParams();
   // 🚨 Get values for r, g, and b from params
   // 🚨 Uncomment next lines after importing
-  // return (
-  // <div
-  //   className={styles.fill}
-  //   style={{ background: `rgb(${r}, ${g}, ${b})` }}
-  // >
-  //   <p>{`rgb(${r},${g},${b})`}</p>
-  // </div>
-  // );
+  return (
+    <div
+      className={styles.fill}
+      style={{ background: `rgb(${r}, ${g}, ${b})` }}
+    >
+      <p>{`rgb(${r},${g},${b})`}</p>
+    </div>
+  );
 }
 
 function ScreenColor() {
-  return <div>{/* Create Route Inside Switch */}</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/rgb/:r/:g/:b">
+          <RGB />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default function App() {
